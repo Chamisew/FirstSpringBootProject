66bb4499a0599111fd74bf328b36a9e648b55808
Chamisew <chamilasewmini2@gmail.com>
2025-01-09 22:58:52 +0530

    delete the instructor(one to one uni)

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -19,8 +19,28 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 
 		return runner->{
-		 	createInstructor(appDAO);
+		 	//findInstructor(appDAO);
+
+			deleteInstructor(appDAO);
 		};
+
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int theId=1;
+		System.out.println("Deleting instructor id:"+theId);
+		appDAO.deleteInstructorById(theId);
+		System.out.println("Done");
+
+	}
+
+	public void findInstructor(AppDAO appDAO){
+
+		int theId=1;
+		System.out.println("Finding instructor id:"+theId);
+		Instructor tempInstructor=appDAO.findInstructorById(theId);
+		System.out.println("tempInstructor:"+tempInstructor);
+		System.out.println("the associated instructorDetail only:"+tempInstructor.getInstructorDetail());
 
 	}
 
