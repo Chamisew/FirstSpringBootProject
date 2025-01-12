c6aab3671910fb7e720d8812c3d961d4584e9c56
Chamisew <chamilasewmini2@gmail.com>
2025-01-12 20:11:09 +0530

    Do some changes to Course class to build up many to many relationship with Student class.

--- 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -38,6 +38,15 @@ public class Course {
     private List<Review> reviews;
 
 
+    @ManyToMany(fetch = FetchType.LAZY,cascade={CascadeType.PERSIST,CascadeType.MERGE,CascadeType.DETACH,CascadeType.REFRESH})
+    @JoinTable(
+                name="course_student",
+                joinColumns=@JoinColumn(name="course_id"),
+                inverseJoinColumns=@JoinColumn(name="student_id")
+    )
+    private List<Student> students;
+
+
     public Course(){
 
     }
@@ -84,6 +93,22 @@ public class Course {
             reviews=new ArrayList<>();
         }
         reviews.add(theReview);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    //add  a convienience method
+    public void addStudent(Student theStudent ){
+        if(students==null){
+            students=new ArrayList<>();
+        }
+        students.add(theStudent);
     }
 
     @Override
